e84c2243a88cd2187386fa09e85558b5aeb1269c
darellanodev <darellano.aliaga@gmail.com>
2024-07-19 20:03:53 +0200

    add animated logo

---
+++ js/logo_animation.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  new Vivus('logo-animation', {
+    duration: 500,
+    start: 'autostart',
+    file: './img/darellanodev_logo.svg',
+  })
+})
