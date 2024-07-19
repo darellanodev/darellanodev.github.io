33940713197d616ee262fe2eb0036f922432bf01
darellanodev <darellano.aliaga@gmail.com>
2024-07-19 20:35:04 +0200

    remove absolute path

--- js/logo_animation.js
+++ js/logo_animation.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
   new Vivus('logo-animation', {
     duration: 500,
     start: 'autostart',
-    file: 'https://darellanodev.github.io/img/darellanodev_logo.svg',
+    file: 'img/darellanodev_logo.svg',
   })
 })
