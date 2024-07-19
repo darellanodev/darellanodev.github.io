6bf6215c584af6e7121fb50bb109390f4622f212
darellanodev <darellano.aliaga@gmail.com>
2024-07-19 20:31:50 +0200

    attempt to set absolute path to .svg to see it in Edge

--- js/logo_animation.js
+++ js/logo_animation.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
   new Vivus('logo-animation', {
     duration: 500,
     start: 'autostart',
-    file: 'img/darellanodev_logo.svg',
+    file: 'https://darellanodev.github.io/img/darellanodev_logo.svg',
   })
 })
