8a3fcd0f44c894815217417c96c840423241157d
darellanodev <darellano.aliaga@gmail.com>
2024-07-19 21:02:50 +0200

    add a delay before showing the animated logo

--- js/logo_animation.js
+++ js/logo_animation.js
@@ -1,7 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
+setTimeout(function () {
+  if (document.readyState === 'complete') {
+    initializeVivus()
+  } else {
+    document.addEventListener('DOMContentLoaded', initializeVivus)
+  }
+}, 2000)
+
+function initializeVivus() {
   new Vivus('logo-animation', {
-    duration: 500,
+    duration: 800,
     start: 'autostart',
     file: 'img/darellanodev_logo.svg',
   })
-})
+}
